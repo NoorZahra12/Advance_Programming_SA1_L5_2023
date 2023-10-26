b5cf778ebf33536c133e9e35a17e1576c5dbaeb9
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2023-10-26 06:10:13 +0400

    demo

--- Chapter1/FurtherExercises/ExerciseIV_countItems.py
+++ Chapter1/FurtherExercises/ExerciseIV_countItems.py
@@ -6,13 +6,14 @@
 
 staff = ["Arshiya", "Usman", "Iftikhar", "Usman", "Rafia", "Mary", "Anmol", "Zainab", "Iftikhar", "Arshiya", "Rafia", "Jake"]
 
-# Initialize an empty dictionary with default values of 0
+# making an empty dictionary with default values of 0, the "item" is the name of the elements in staff
 staff_dictionary = {item: 0 for item in staff}
 
-# Count the occurrences of each item and increasing the value of key by 1 in the the dictionary
+# Counting the appearance of each "item" in the staff list and increasing the value of key by 1 in the the dictionary
+# it will increase the value of existing key by one instead of adding another similar key with value 1
 for item in staff:
     staff_dictionary[item] += 1
 
 #displaying the dictionary using for loop
 for name, count in staff_dictionary.items():
-    print(name,count)
+    print("The name:",name,", appeared",count,"times")
