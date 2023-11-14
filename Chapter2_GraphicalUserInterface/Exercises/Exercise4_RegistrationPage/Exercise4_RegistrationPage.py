37a405de0ae93129f3a91db57d6455ddb35652a4
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2023-11-14 11:00:05 +0400

    Update Exercise4_RegistrationPage.py
    just commenting and fixing variable names so it's easier to read the code

--- Chapter2_GraphicalUserInterface/Exercises/Exercise4_RegistrationPage/Exercise4_RegistrationPage.py
+++ Chapter2_GraphicalUserInterface/Exercises/Exercise4_RegistrationPage/Exercise4_RegistrationPage.py
@@ -1,9 +1,7 @@
 # Exercise 4: Registration page ☑️
 # Using widgets create a GUI as shown in below image
 
-# Exercise 4: Registration page ☑️
-# Using widgets create a GUI as shown in below image
-
+# importing all the libraries
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
@@ -13,35 +11,45 @@ root = tk.Tk()
 root.title("12/nov/2023")
 root.configure(bg="white")
 
+# making a function for the submit button
 def submit():
+    # getting all the input written by user and putting it inside a variable
     stdname = studentNameEntry.get()
     stdmobnum = mobileNumEntry.get()
     stdemail = emailEntry.get()
     stdhome = homeAddrEntry.get()
-    stdgender = selected_option.get()
-    stdcourse = radio_var.get()
+    stdgender = gender_selection_option.get()
+    stdcourse = courses_radio_var.get()
 
     # if not stdname or not stdmobnum or not stdemail or not stdhome or not stdgender or not stdcourse:
     if not all((stdname, stdmobnum, stdemail, stdhome, stdgender, stdcourse)):
+        # in case the user leaves a these specific fields empty, the text will show incomplete
         submit_button["text"] = "Incomplete"
+        # here after 3000 ms(milisecond) or 3 seconds the text will change to Submit
         root.after(3000, lambda: submit_button.config(text="Submit"))
     else:
+        # if user types everthing correctly  it will change the text t submitted
         submit_button["text"] = "Submitted"
+        # here after 3000 ms(milisecond) or 3 seconds the text will change to Submit
         root.after(3000, lambda: submit_button.config(text="Submit"))
 
+# making a function for the clear button
 def clear():
+    # clearing everthing
     studentNameEntry.delete(0, "end")
     mobileNumEntry.delete(0, "end")
     emailEntry.delete(0, "end")
     homeAddrEntry.delete(0, "end")
-    selected_option.set("")
-    radio_var.set("")
-    checkbox_var_1.set(False)
-    checkbox_var_2.set(False)
-    checkbox_var_3.set(False)
+    gender_selection_option.set("")
+    courses_radio_var.set("")
+    eng_checkbox_var_1.set(False)
+    tagalog_checkbox_var_2.set(False)
+    urdu_hindi_checkbox_var_3.set(False)
 
 
 ############### LOGO IMAGE ####################
+# i couldn't add the image the normal way cause it wasn't accepting "BSULOGO.png". 
+# I'm sure there's a better way or a simpler way to do the same code but this is the only way i could figure out for now.
 script_directory = os.path.dirname(os.path.realpath(__file__))
 img = Image.open(os.path.join(script_directory, "BSULOGO.png"))
 tk_img = ImageTk.PhotoImage(img)
@@ -52,9 +60,9 @@ logolabel.pack(side="top")
 
 #################### Registration Page #########################
 
+# making a container where i will put everything inside it
 container = tk.Frame(root)
 container.pack()
-button_container = tk.Frame(container)
 
 # Heading texts
 text1 = tk.Label(container, text="Student Management System", font=("Helvetica", 16), fg="#22363D")
@@ -96,23 +104,23 @@ genderLabel = tk.Label(container, text="Gender")
 genderLabel.grid(row=6, column=0)
 
 gender_options = ['Male', 'Female']
-selected_option = tk.StringVar()
-studentGenderDropdown = ttk.Combobox(container, textvariable=selected_option, values=gender_options, state="readonly")
+gender_selection_option = tk.StringVar()
+studentGenderDropdown = ttk.Combobox(container, textvariable=gender_selection_option, values=gender_options, state="readonly")
 studentGenderDropdown.grid(row=6, column=1)
 
 
 # Courses Enrolled
 courseLabel = tk.Label(container, text="Courses Enrolled")
 courseLabel.grid(row=7, column=0)
-radio_var = tk.StringVar(value=0)
+courses_radio_var = tk.StringVar(value=0)
 # Radio buttons
-radio_1 = tk.Radiobutton(container, text="BSc CC", variable=radio_var, value=1)
+radio_1 = tk.Radiobutton(container, text="BSc CC", variable=courses_radio_var, value=1)
 radio_1.grid(row=7, column=1)
-radio_2 = tk.Radiobutton(container, text="BSc CY", variable=radio_var, value=2)
+radio_2 = tk.Radiobutton(container, text="BSc CY", variable=courses_radio_var, value=2)
 radio_2.grid(row=8, column=1)
-radio_3 = tk.Radiobutton(container, text="BSc PSY", variable=radio_var, value=3)
+radio_3 = tk.Radiobutton(container, text="BSc PSY", variable=courses_radio_var, value=3)
 radio_3.grid(row=9, column=1)
-radio_4 = tk.Radiobutton(container, text="BA & BM", variable=radio_var, value=4)
+radio_4 = tk.Radiobutton(container, text="BA & BM", variable=courses_radio_var, value=4)
 radio_4.grid(row=10, column=1)
 
 
@@ -120,34 +128,39 @@ radio_4.grid(row=10, column=1)
 langLabel = tk.Label(container, text="Languages Known")
 langLabel.grid(row=11, column=0)
 
-checkbox_var_1 = tk.BooleanVar()
-checkbox_var_2 = tk.BooleanVar()
-checkbox_var_3 = tk.BooleanVar()
+eng_checkbox_var_1 = tk.BooleanVar()
+tagalog_checkbox_var_2 = tk.BooleanVar()
+urdu_hindi_checkbox_var_3 = tk.BooleanVar()
 # Checkboxes
-checkbox_eng = tk.Checkbutton(container, text="English", variable=checkbox_var_1)
+checkbox_eng = tk.Checkbutton(container, text="English", variable=eng_checkbox_var_1)
 checkbox_eng.grid(row=11, column=1)
 
-checkbox_tagalog = tk.Checkbutton(container, text="Tagalog", variable=checkbox_var_3)
+checkbox_tagalog = tk.Checkbutton(container, text="Tagalog", variable=urdu_hindi_checkbox_var_3)
 checkbox_tagalog.grid(row=11, column=2)
 
-checkbox_urdu_hindi = tk.Checkbutton(container, text="Hindi/Urdu", variable=checkbox_var_2)
+checkbox_urdu_hindi = tk.Checkbutton(container, text="Hindi/Urdu", variable=tagalog_checkbox_var_2)
 checkbox_urdu_hindi.grid(row=12, column=1)
 
+
+############################## ENGLISH SCALE ##############################
+# label
 rateEngCommSkillsLabel = tk.Label(container, text="Rate Your English Communication Skills: ")
 rateEngCommSkillsLabel.grid(row=13, columnspan=5)
-
 slider = tk.IntVar()
 rateEngCommSkillsScale = tk.Scale(container, from_=0, to=10, orient='horizontal', variable=slider, length=200)
-
 # Configure the Scale widget with a blue background
 ttk.Style().configure("TScale", troughcolor="lightblue")
 ttk.Scale(container, from_=0, to=10, orient='horizontal', variable=slider, style="TScale").grid(row=14, columnspan=5)
 
+######################## BUTTONS ########################
+# submit button
 submit_button = tk.Button(container, text="Submit", command=submit, bg="#22263D", fg="white", padx=20, pady=5)
-submit_button.grid(row=15, column=0, columnspan=2, padx=(5, 10), pady=(5, 10))
+# here i can add padding on the outer space of the widgets (similar to adding margin in html/css the inner padding is written above)
+submit_button.grid(row=15, column=0, columnspan=2, padx=(10, 10), pady=(8, 10))
 
+# clear button
 clear_button = tk.Button(container, text="Clear", command=clear, bg="#22263D", fg="white", padx=20, pady=5)
-clear_button.grid(row=15, column=1, columnspan=2, padx=(10, 10), pady=(10, 10))
+clear_button.grid(row=15, column=1, columnspan=2, padx=(10, 10), pady=(8, 10))
 
 root.geometry(f"{img.width}x600")
 
